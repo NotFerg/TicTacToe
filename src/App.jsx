a24a3733732eed48cf8d78b24e00deea06cbe7b6
Fergus <AFR0155@DLSUD.EDU.PH>
2024-02-22 21:50:20 +0800

    added gameboard currently only X can be clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import Player from "./assets/components/Player/Player.jsx";
+import GameBoard from "./assets/components/GameBoard/GameBoard.jsx";
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="0"/>
+        
+        </ol>
+       <GameBoard/>
+      </div>
+      Log
+    </main>
+  );
 }
 
-export default App
+export default App;
